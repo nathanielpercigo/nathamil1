1d9963cbb5a63bce7345192c9a359837bfff0b91
nathanielpercigo <161479561+nathanielpercigo@users.noreply.github.com>
2024-10-30 07:52:36 -0300

    a

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ function criaCartao (categoria, pergunta, resposta){
 let respostaEstaVisivel = false;
 
 function viraCartao(){
-    respostaEstaVisivel
+    respostaEstaVisivel = !respostaEstaVisivel;
     cartao.classList.toggle('active', respostaEstaVisivel);
 }
-    
+cartao.addEventListener('click', viraCartao);
 
 container.appendChild(cartao);
 }
