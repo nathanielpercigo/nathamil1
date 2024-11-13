25ba272be2bc245ff3fcc116d4f2be986de4e4b7
nathanielpercigo <161479561+nathanielpercigo@users.noreply.github.com>
2024-11-13 08:11:41 -0300

    a

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = 
+    cartao.innerHTML = `
     <div class="conteudo-cartao">
         <h3>${categoria}</h3>
     <div class="pergunta-cartao">
@@ -12,6 +12,7 @@ function criaCartao (categoria, pergunta, resposta){
         <p>${resposta}</p>
     </div>
     </div>
+`
 
 let respostaEstaVisivel = false;
 
